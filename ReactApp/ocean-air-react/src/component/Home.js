5796cd8c3c5aeaec80a0d55e343920d952d7aaf9
Zkellogg <59624561+Zkellogg@users.noreply.github.com>
2021-10-11 12:01:44 -0400

    about us

--- ReactApp/ocean-air-react/src/component/Home.js
+++ ReactApp/ocean-air-react/src/component/Home.js
@@ -7,21 +7,34 @@ const Home = (props) => {
   return (
     <div className="homepage-container">
       <div className="imageDiv">
-      <SignIn 
-      isSignInClicked={props.isSignInClicked}
-      isSignUpClicked={props.isSignUpClicked}
-      />
+        <SignIn
+          isSignInClicked={props.isSignInClicked}
+          isSignUpClicked={props.isSignUpClicked}
+        />
       </div>
-      <div id="aboutUs" className="aboutUs"></div>
+      <div id="aboutUs" className="aboutUs">
+        <h1>
+          <strong>About Us</strong>
+        </h1>
+        <h3> -Our Story- </h3>
+        <h5>
+          pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas maecenas pharetra convallis posuere morbi leo
+          urna molestie at elementum eu facilisis sed odio morbi quis commodo
+          odio aenean sed adipiscing diam et netus et malesuada fames ac turpis
+          egestas maecenas pharetra convallis posuere morbi leo urna molestie at
+          elementum eu facilisis sed
+        </h5>
+      </div>
     </div>
   );
 };
 
-const mapStateToProps=(state)=>{
-  return{
-    isSignInClicked:state.isSignInActive,
-    isSignUpClicked:state.isSignUpActive
-  }
-}
+const mapStateToProps = (state) => {
+  return {
+    isSignInClicked: state.isSignInActive,
+    isSignUpClicked: state.isSignUpActive,
+  };
+};
 
 export default connect(mapStateToProps)(Home);
--- ReactApp/ocean-air-react/src/store/reducer.js
+++ ReactApp/ocean-air-react/src/store/reducer.js
@@ -1,34 +1,27 @@
-const initialState={
-    isSignInActive:false,
-    isSignUpActive:false
-}
+const initialState = {
+  isSignInActive: false,
+  isSignUpActive: false,
+};
 
-const reducer=(state=initialState,action)=>{
-    if(action.type=='ON_SIGNINBTN_CLICKED'){
-        return{...state,
-            isSignInActive:action.payload,
-            isSignUpActive:false
-        }
-    }else if(action.type=='ON_SIGNUPBTN_CLICKED'){
-        return{...state,
-            isSignUpActive:action.payload,
-            isSignInActive:false
-        }
-    }else if(action.type=='OPEN_SIGNIN_WINDOW'){
-        return{
-            ...state,
-            isSignUpActive:false,
-            isSignInActive:action.payload
-        }
+const reducer = (state = initialState, action) => {
+  if (action.type === "ON_SIGNINBTN_CLICKED") {
+    return { ...state, isSignInActive: action.payload, isSignUpActive: false };
+  } else if (action.type === "ON_SIGNUPBTN_CLICKED") {
+    return { ...state, isSignUpActive: action.payload, isSignInActive: false };
+  } else if (action.type === "OPEN_SIGNIN_WINDOW") {
+    return {
+      ...state,
+      isSignUpActive: false,
+      isSignInActive: action.payload,
+    };
+  } else if (action.type === "OPEN_SIGNUP_WINDOW") {
+    return {
+      ...state,
+      isSignInActive: false,
+      isSignUpActive: action.payload,
+    };
+  }
+  return state;
+};
 
-    }else if(action.type=='OPEN_SIGNUP_WINDOW'){
-        return{
-            ...state,
-            isSignInActive:false,
-            isSignUpActive:action.payload
-        }
-    }
-    return state
-}
-
-export default reducer
+export default reducer;
