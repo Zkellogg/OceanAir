f341b89915610b29e8dff2ff5900c54434b736a0
Kai Jenkins <86726403+KMJENK17@users.noreply.github.com>
2021-10-13 11:47:10 -0400

    adding slidehow

--- ReactApp/ocean-air-react/src/component/Seagrove.js
+++ ReactApp/ocean-air-react/src/component/Seagrove.js
@@ -3,9 +3,21 @@ import "../CSS/Property.css"
 
 function Seagrove(){
     return(
-        <div>
-            <h2>Seagrove Page</h2>
+        <div class="row">
+        <div class="column">
+          <img src="../src/images/SantaRosa/day-1.jpeg" onclick="openModal();currentSlide(1)" class="hover-shadow" alt=""/>
         </div>
+        <div class="column">
+          <img src="ReactApp/ocean-air-react/src/images/SantaRosa/beach-2.jpeg" onclick="openModal();currentSlide(2)" class="hover-shadow" alt=""/>
+        </div>
+        <div class="column">
+          <img src="ReactApp/ocean-air-react/src/images/SantaRosa/livingroom-3.jpeg" onclick="openModal();currentSlide(3)" class="hover-shadow" alt=""/>
+        </div>
+        <div class="column">
+          <img src="ReactApp/ocean-air-react/src/images/SantaRosa/kitchen-1.jpeg" onclick="openModal();currentSlide(4)" class="hover-shadow" alt=""/>
+        </div>
+      </div>
+      
     )
 }
 
