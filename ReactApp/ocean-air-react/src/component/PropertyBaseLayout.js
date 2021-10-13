97634c130ddf2d1154d0a33a267b11fc3932459c
DerekDunlap <derekdunlapjr@gmail.com>
2021-10-13 11:49:37 -0400

    Edited styling for Reviews Component

--- ReactApp/ocean-air-react/src/component/PropertyBaseLayout.js
+++ ReactApp/ocean-air-react/src/component/PropertyBaseLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Review from "./Review";
 import "../CSS/Navbar.css"
-import "../CSS/Contact.css"
+import "../CSS/Review.css"
 
 function BaseLayoutProperty(props){
     return(
