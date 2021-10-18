fc3502064486c0594e47608c6eee0b9268efd96a
Zkellogg <59624561+Zkellogg@users.noreply.github.com>
2021-10-18 15:43:18 -0400

    css changes

--- ReactApp/ocean-air-react/src/component/Seagrove.js
+++ ReactApp/ocean-air-react/src/component/Seagrove.js
@@ -1,24 +1,20 @@
 import React from "react";
-import "../CSS/Property.css"
-import PictureGalleryComponent from "../component/PictureGalleryComponent"
-import Button from 'react-bootstrap/Button'
+import "../CSS/Property.css";
+import PictureGalleryComponent from "../component/PictureGalleryComponent";
+import Button from "react-bootstrap/Button";
 function Seagrove() {
-    
-    
-    
-    return (
-        
-        <div>
-            <h2 class="title">Ocean Air on 30A</h2>
-            <a href='https://oceanair.guestybookings.com/listings/6001d148766a14002e1e2174'>
-            <Button variant="primary" className="bookNowButton">Book Now</Button>
-            </a>
-            <PictureGalleryComponent />
-            <p class="clickForMore">Click image to view more images</p>
-            
-        </div>
-        
-    )
+  return (
+    <div>
+      <h2 class="title">Ocean Air on 30A</h2>
+      <a href="https://oceanair.guestybookings.com/listings/6001d148766a14002e1e2174">
+        <Button variant="primary" className="bookNowButton">
+          Book Now
+        </Button>
+      </a>
+      <PictureGalleryComponent />
+      <p class="clickForMore">Click image to view more images</p>
+    </div>
+  );
 }
 
-export default Seagrove
+export default Seagrove;
