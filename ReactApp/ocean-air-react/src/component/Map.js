24af97b6f9b35ca8bbcae6ba188c666c1d045919
DerekDunlap <derekdunlapjr@gmail.com>
2021-10-19 15:18:10 -0400

    Integrated with Map feature on property pages

--- ReactApp/ocean-air-react/src/component/Map.js
+++ ReactApp/ocean-air-react/src/component/Map.js
@@ -3,8 +3,8 @@ import "../CSS/App.css"
 
 function Map() {
     return (
-        <iframe id="map-iframe" width="600" height="450" loading="lazy" allowfullscreen
-            src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAsSuhDzEqib-0sYnkaJBtS_wDHqlanXLQ&q=13 Eastern Lake Ct,Santa Rosa+Florida"}>
+        <iframe id="map-iframe" width="500" height="450" loading="lazy" allowfullscreen
+            src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAsSuhDzEqib-0sYnkaJBtS_wDHqlanXLQ&q=13+Eastern+Lake+Ct,Santa Rosa+Florida"}>
         </iframe>
         )
 }
--- ReactApp/ocean-air-react/src/component/Pompano.js
+++ ReactApp/ocean-air-react/src/component/Pompano.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import ImageSlider from "./ImageSlider";
 import "../CSS/Pompano.css";
+import Map from "./Map";
 
 function Pompano() {
   return (
@@ -11,8 +12,8 @@ function Pompano() {
         <Button variant="primary" className="bookNowButton">
           Book Now
         </Button>
-        </div>
-        <div>
+      </div>
+      <div>
         <ImageSlider />
       </div>
       <div className="main">
@@ -22,49 +23,50 @@ function Pompano() {
             Ocean Air is a beachfront duplex divided into 4
             units. Each unit has a panoramic view of the beach with a private
             beachfront deck. </p> <p> Each unit has a full kitchen, living room, dining
-            room and has two bedrooms and two full bathrooms. All the units are
-            connected through an interior foyer, making multiple unit rentals
-            perfect for family reunions and weddings.</p> <p> Located directly on the
-            beach and south of Hwy 30A, these newly renovated units include
-            features like upscale stainless steel appliances and granite
-            surfaces, new bathrooms, full smart home integrations including,
-            swivel from ceiling jumbo TV mounts, and smart audio devices! Each
-            unit features reverse-osmosis water purification systems to refill
-            your bottles and save our beach from un-needed plastic. </p><p> The living
-            room is decorated with furniture including a jumbo Lovesac beanbag
-            chair placed in front of the picture windows. The most prominent
-            feature of this location is the private beach. Ocean Air 30A boasts
-            of 70’ of private beach, giving each beach chair set a direct ocean
-            view without outside interference. </p><p>Each unit is guaranteed one set
-            of chairs and an umbrella with the Ocean Air-logo. Additional chair
-            sets are available for an additional fee. Off the main living area
-            of each unit is a private beachfront covered deck. The Trex cocktail
-            tables and chairs are perfect for having lunch overlooking the sand
-            or for evening cocktails. Each deck connects to the private beach
-            walkover. The walkover includes two large reading nooks overlooking
-            the beautiful Gulf of Mexico. Evening sunsets are spectacular from
-            this perch, as are the morning sunrises. Many beautiful beach
-            photographs have been taken from there.</p><p> Weekly rentals include use
-            of our paddle boards and kayaks, and brand new beach bicycles (first
-            come first served). Gas grills are also provided in the front of the
-            house. Our extra-large parking area affords our renters with a
-            minimum of two parking spaces each. </p><p>Dogs and Cats are accepted with
-            an additional $200 cleaning fee and must be disclosed prior to
-            arrival. The $30 damage insurance fee does not cover pet-related
-            damages. Any damages over $500 will be charged to the guest. Air
-            mattress available on request.
+              room and has two bedrooms and two full bathrooms. All the units are
+              connected through an interior foyer, making multiple unit rentals
+              perfect for family reunions and weddings.</p> <p> Located directly on the
+                beach and south of Hwy 30A, these newly renovated units include
+                features like upscale stainless steel appliances and granite
+                surfaces, new bathrooms, full smart home integrations including,
+                swivel from ceiling jumbo TV mounts, and smart audio devices! Each
+                unit features reverse-osmosis water purification systems to refill
+                your bottles and save our beach from un-needed plastic. </p><p> The living
+                  room is decorated with furniture including a jumbo Lovesac beanbag
+                  chair placed in front of the picture windows. The most prominent
+                  feature of this location is the private beach. Ocean Air 30A boasts
+                  of 70’ of private beach, giving each beach chair set a direct ocean
+                  view without outside interference. </p><p>Each unit is guaranteed one set
+                    of chairs and an umbrella with the Ocean Air-logo. Additional chair
+                    sets are available for an additional fee. Off the main living area
+                    of each unit is a private beachfront covered deck. The Trex cocktail
+                    tables and chairs are perfect for having lunch overlooking the sand
+                    or for evening cocktails. Each deck connects to the private beach
+                    walkover. The walkover includes two large reading nooks overlooking
+                    the beautiful Gulf of Mexico. Evening sunsets are spectacular from
+                    this perch, as are the morning sunrises. Many beautiful beach
+                    photographs have been taken from there.</p><p> Weekly rentals include use
+                      of our paddle boards and kayaks, and brand new beach bicycles (first
+                      come first served). Gas grills are also provided in the front of the
+                      house. Our extra-large parking area affords our renters with a
+                      minimum of two parking spaces each. </p><p>Dogs and Cats are accepted with
+                        an additional $200 cleaning fee and must be disclosed prior to
+                        arrival. The $30 damage insurance fee does not cover pet-related
+                        damages. Any damages over $500 will be charged to the guest. Air
+                        mattress available on request.
           </p>
         </div>
         <div className="aminities">
-            <h2>This property offers</h2>
-            <ul>
-                <li>100% Private beach</li>
-                <li>Private two king bedroom unit</li>
-                <li>Full Kitchen with brand-new appliences</li>
-                <li>Free prking for up to two vehicles</li>
-                <li>Full washer and dryer</li>
-                <li>Two full bathrooms</li>
-            </ul>
+          <Map />
+          <h2>This property offers</h2>
+          <ul>
+            <li>100% Private beach</li>
+            <li>Private two king bedroom unit</li>
+            <li>Full Kitchen with brand-new appliences</li>
+            <li>Free prking for up to two vehicles</li>
+            <li>Full washer and dryer</li>
+            <li>Two full bathrooms</li>
+          </ul>
         </div>
       </div>
     </div>
